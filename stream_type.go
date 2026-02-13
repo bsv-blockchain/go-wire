93d619bc86d42adf0453fcac51b9edd1457e5596
liam <26772+liam@users.noreply.github.com>
2026-02-13 18:42:52 +0000

    fix lint errors

--- stream_type.go
+++ stream_type.go
@@ -4,12 +4,18 @@ package wire
 type StreamType uint8
 
 const (
+	// StreamTypeUnknown is an unknown stream type.
 	StreamTypeUnknown StreamType = 0
+	// StreamTypeGeneral is a general stream type.
 	StreamTypeGeneral StreamType = 1
-	StreamTypeData1   StreamType = 2
-	StreamTypeData2   StreamType = 3
-	StreamTypeData3   StreamType = 4
-	StreamTypeData4   StreamType = 5
+	// StreamTypeData1 is a data stream type.
+	StreamTypeData1 StreamType = 2
+	// StreamTypeData2 is a data stream type.
+	StreamTypeData2 StreamType = 3
+	// StreamTypeData3 is a data stream type.
+	StreamTypeData3 StreamType = 4
+	// StreamTypeData4 is a data stream type.
+	StreamTypeData4 StreamType = 5
 )
 
 // MaxAssociationIDLen is the maximum allowed length for an association ID
